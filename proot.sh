83fb394f53d1680148bb0da90583628097508b32
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2024-07-01 20:11:02 +0200

    proot.sh: fix arm32 support

--- proot.sh
+++ proot.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# arch output
+arch=$(uname -m)
+
 # Unofficial Bash Strict Mode
 set -euo pipefail
 IFS=$'\n\t'
@@ -80,6 +83,10 @@ EOF
 mkdir $PREFIX/var/lib/proot-distro/installed-rootfs/debian/home/$username/.fonts/
 cp .fonts/NotoColorEmoji-Regular.ttf $PREFIX/var/lib/proot-distro/installed-rootfs/debian/home/$username/.fonts/ 
 
-#Setup Hardware Acceleration
-pd login debian --shared-tmp -- env DISPLAY=:1.0 wget https://github.com/phoenixbyrd/Termux_XFCE/raw/main/mesa-vulkan-kgsl_24.1.0-devel-20240120_arm64.deb
-pd login debian --shared-tmp -- env DISPLAY=:1.0 sudo apt install -y ./mesa-vulkan-kgsl_24.1.0-devel-20240120_arm64.deb
+#Setup Hardware Acceleration (Not available for arm32)
+if echo "$arch" | grep -q 'armv[7-9]l$'; then
+	echo "Arm32 detected! Hardware Acceleration on PRoot is not available for this system." 
+else [ "$arch" = "aarch64" ]; then
+	pd login debian --shared-tmp -- env DISPLAY=:1.0 wget https://github.com/phoenixbyrd/Termux_XFCE/raw/main/mesa-vulkan-kgsl_24.1.0-devel-20240120_arm64.deb
+	pd login debian --shared-tmp -- env DISPLAY=:1.0 sudo apt install -y ./mesa-vulkan-kgsl_24.1.0-devel-20240120_arm64.deb
+fi
